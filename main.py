827f2fdebaa6c03aecf4ad9e8d0b64887af14da7
miguel-kjh <miguelangel.kjh@gmail.com>
2022-07-27 13:34:57 +0100

    update

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import os
 
 import warnings
 
+import yaml
+
 from service.CleanDataService import CleanDataService
 from service.TransformDialogueService import TransformDialogueService
+from utils.ProjectConstants import REPOSITORY_PATH
 from view.Logger import Logger
 
 warnings.filterwarnings("ignore", ".*")
@@ -18,7 +21,6 @@ def parse_args():
 
 
 def main():
-
     args = parse_args()
 
     for action in args.__dict__:
@@ -29,6 +31,19 @@ def main():
                 CleanDataService().process()
 
 
-
 if __name__ == "__main__":
-    main()
+    #main()
+    from datasets import load_dataset
+
+    dataset = load_dataset("multi_woz_v22")
+    example = dataset['test'].to_dict()['turns'][0]
+    print(example.keys())
+    print(example)
+    print("FRAMES")
+    print(yaml.dump(example['frames']))
+    print("ACTS")
+    print(yaml.dump(example['dialogue_acts']))
+    """
+     turns: los dialogos -> lista
+     'turn_id', 'speaker', 'utterance', 'frames', 'dialogue_acts'
+    """
--- utils/mongo_db_utils.py
+++ utils/mongo_db_utils.py
@@ -1,3 +1,1 @@
-username = "miguel"
-password = "mates.108"
 LOCAL_SERVER = "mongodb://localhost:27017"
