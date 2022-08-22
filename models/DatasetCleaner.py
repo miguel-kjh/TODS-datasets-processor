8f57406f59edd8bcafcf15fdd5466b77dea75fef
miguel-kjh <miguelangel.kjh@gmail.com>
2022-08-22 08:46:34 +0100

    little refactoring

--- models/DatasetCleaner.py
+++ models/DatasetCleaner.py
@@ -13,13 +13,13 @@ from utils.utils import list2atomic_item
 class DatasetCleaner:
 
     def __init__(self):
-        self._dummy_mark = '_INTENT'
         self._id_name = 'Dialogue Id'
         self._intent_name = 'Intents'
         self._action_name = 'Actions'
         self._speaker_name = 'Speaker'
         self._type_name = 'Type'
         self._service_name = 'Service'
+        self._task_name = 'Original_Intents'
 
         self.dummy_acton = 'LISTEN'
 
@@ -45,26 +45,33 @@ class DatasetCleaner:
         )
 
         for id, df in tqdm(dataset.groupby(by="Dialogue Id"), desc="Cleaning datasets..."):
-            for i in range(0, len(df), 2):
-                row_1 = df.iloc[i]
-                row_2 = df.iloc[i + 1]
 
-                actions = copy(row_2[self._action_name])
-                actions.append(self.dummy_acton)
-                atomic_action = list2atomic_item(row_2[self._action_name])
-                atomic_action.append(self.dummy_acton)
+            cond = 0
+            for col in [self._intent_name, self._action_name, self._service_name]:
+                df_mask = df[col].apply(lambda x: "" if type(x) == list and len(x) == 0 else x) == ""
+                cond += df_mask.sum()
 
-                self.schemaDatabase.add_dialogue_id(id)
-                self.schemaDatabase.add_domain(row_1[self._service_name])
-                self.schemaDatabase.add_task(row_1["Original_Intents"])
-                self.schemaDatabase.add_user_utterance(row_1["Text"])
-                self.schemaDatabase.add_intention(row_1[self._intent_name])
-                self.schemaDatabase.add_atomic_intent(list2atomic_item(row_1[self._intent_name]))
-                self.schemaDatabase.add_slots(row_1["Slot"])
-                self.schemaDatabase.add_slots_value(row_1["Slot_values"])
-                self.schemaDatabase.add_bot_response(row_2["Text"])
-                self.schemaDatabase.add_action(actions)
-                self.schemaDatabase.add_atomic_action(atomic_action)
-                self.schemaDatabase.add_type(row_2[self._type_name])
+            if cond == 0:
+                for i in range(0, len(df), 2):
+                    row_1 = df.iloc[i]
+                    row_2 = df.iloc[i + 1]
+
+                    actions = copy(row_2[self._action_name])
+                    actions.append(self.dummy_acton)
+                    atomic_action = list2atomic_item(row_2[self._action_name])
+                    atomic_action.append(self.dummy_acton)
+
+                    self.schemaDatabase.add_dialogue_id(id)
+                    self.schemaDatabase.add_domain(row_1[self._service_name])
+                    self.schemaDatabase.add_task(row_1["Original_Intents"])
+                    self.schemaDatabase.add_user_utterance(row_1["Text"])
+                    self.schemaDatabase.add_intention(row_1[self._intent_name])
+                    self.schemaDatabase.add_atomic_intent(list2atomic_item(row_1[self._intent_name]))
+                    self.schemaDatabase.add_slots(row_1["Slot"])
+                    self.schemaDatabase.add_slots_value(row_1["Slot_values"])
+                    self.schemaDatabase.add_bot_response(row_2["Text"])
+                    self.schemaDatabase.add_action(actions)
+                    self.schemaDatabase.add_atomic_action(atomic_action)
+                    self.schemaDatabase.add_type(row_2[self._type_name])
 
         return pd.DataFrame(self.schemaDatabase.get_dataset_schema())
