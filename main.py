71445dc363a85cbdf3ef4477f49c7e7fb2446ebb
miguel-kjh <miguelangel.kjh@gmail.com>
2022-09-01 12:34:12 +0100

    added seed to dataset processor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import warnings
+import random
+
 import hydra
+import numpy as np
 from omegaconf import DictConfig
 
 from service.CleanDataService import CleanDataService
@@ -8,8 +11,14 @@ from service.TransformDialogueService import TransformDialogueService
 warnings.filterwarnings("ignore", ".*")
 
 
+def reset_seed(seed: int) -> None:
+    random.seed(seed)
+    np.random.seed(seed)
+
+
 @hydra.main(config_path="conf", config_name="config", version_base=None)
 def main(cfg: DictConfig) -> None:
+    reset_seed(cfg.seed)
     if cfg.operation == "download":
         TransformDialogueService(cfg).process()
     elif cfg.operation == "clean":
